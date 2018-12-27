8060b54f7ab1ff049217a509448ecb77c3fbbc31
SeacenLiu <1016667972@qq.com>
2018-12-27 20:41:29 +0800

    fix ignore

---
+++ RxTravelHere/Source/Record/Record.Edit/Record.Edit.View.swift
@@ -0,0 +1,15 @@
+//
+//  Record.Edit.View.swift
+//  RxTravelHere
+//
+//  Created by SeacenLiu on 2018/12/27.
+//  Copyright © 2018 SeacenLiu. All rights reserved.
+//
+
+import UIKit
+
+extension Record.Edit {
+    internal class View: UIViewController {
+        
+    }
+}
---
+++ RxTravelHere/Source/Record/Record.Edit/Record.Edit.ViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  Record.Edit.ViewModel.swift
+//  RxTravelHere
+//
+//  Created by SeacenLiu on 2018/12/27.
+//  Copyright © 2018 SeacenLiu. All rights reserved.
+//
+
+import Foundation
+
+extension Record.Edit {
+    internal class ViewModel {
+        
+    }
+}
